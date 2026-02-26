d44a8acb6aa02566d8098e443f6d6e242738d893
Pandu-1 <pandusurya456@gmail.com>
2026-02-26 16:56:47 +0530

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
-import './index.css'
+import ComponentOne from './Components/Component1';
+import './App.css'
 
 import React,{useState} from "react";
 
@@ -8,48 +9,12 @@ import React,{useState} from "react";
 
 
 const App = () => {
-
- const [count,setCount]=useState(0);
-
-
-const increment=()=>{
-
-  setCount((count)=>{
-    return count+1;
-  });
-  setCount((count)=>{
-    return count+1;
-  })
- 
-
-}
-
-const decrement=()=>
-  setCount(count-1);
-
- 
-  return (
+  return(
     <div>
-      <button onClick={decrement}>-</button>
-      <span>{count}</span>
-      <button onClick={increment}>+</button>
+      <ComponentOne/>
     </div>
-  
   )
+ 
 }
-
-
-
-  {/* <li className='comment'>
-      <div className='comments-header'>
-        <h3 className='email'>email@email.com</h3>
-        <h3 className='name'>some name random name</h3>
-      </div>
-      <p className='message'>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-         Fugit nemo beatae delectus ipsam tenetur dolore! Hic quia voluptate laboriosam
-         molestias esse incidunt rem, cumque fuga culpa deleniti saepe minima possimus?
-      </p>
-    </li> */}
 
 export default App
--- src/Components/Component1.jsx
+++ src/Components/Component1.jsx
@@ -1,9 +1,46 @@
 import React from "react";
 
-function ComponentOne({names,phone,city}){
+import {useState} from "react";
+
+function ComponentOne(){
+
+    const initialObject={
+        name:"surya",
+        age:21,
+        lastname:"yashu"
+    }
+
+    const [data,setData]=useState(initialObject)
+
+    const changeName=()=>{
+        setData({
+                ...data,
+                name:"chowdary"
+            });
+    }
+
+    const changeLastName=()=>{
+        setData({
+            ...data,
+            lastname:"vadisana"
+        });
+    }
+
+    const changeAge=()=>{
+        setData({
+            ...data,
+            age:10
+        })
+    }
+
     return (
-        <div>
-            
+        <div className="element">
+            <h1>my name is {data.name}</h1>
+            <button onClick={changeName}>change name</button>
+            <h1>my name is {data.lastname}</h1>
+            <button onClick={changeLastName}>change lastName</button>
+            <h1>my age is {data.age}</h1>
+            <button onClick={changeAge}>change age</button>
         </div>
     )
 }
